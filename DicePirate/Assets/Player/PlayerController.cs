6e231bdff3d80d055926b790e6d23c6a36de8f5f
SHERmaan <igor.derbis@gmail.com>
2014-06-30 20:00:34 +0200

    no message

---
+++ DicePirate/Assets/Player/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	private float rotationSpeed = 100;
+	public float velocity = 0;
+	private float maxSpeed = 5;
+	public float Leap = 0;
+	public GameObject shipModel;
+
+	private bool useDicepl = false;
+	// Use this for initialization
+	void Start () {
+		velocity = maxSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!useDicepl) 
+		{
+			if (Input.GetKey (KeyCode.A))
+			{
+				Leap = -1;
+			}
+			else if (Input.GetKey (KeyCode.D))
+			{
+				Leap = 1;
+			}
+			else 
+			{
+				Leap = 0;
+			}
+		}
+
+		shipModel.transform.rotation = Quaternion.Euler(new Vector3(shipModel.transform.eulerAngles.x, shipModel.transform.eulerAngles.y, 0));
+		shipModel.transform.Rotate(new Vector3(0.0f, Leap * rotationSpeed * Time.deltaTime, 0));
+		shipModel.transform.rotation = Quaternion.Euler(new Vector3(shipModel.transform.eulerAngles.x, shipModel.transform.eulerAngles.y, -Leap*10.0f));
+		transform.Translate(shipModel.transform.forward*velocity*Time.deltaTime);
+	}
+}
---
+++ DicePirate/Assets/Player/SmoothFollowCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothFollowCamera : MonoBehaviour {
+	public Transform target;
+	private Vector3 targetOffset = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		targetOffset = target.position - this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
